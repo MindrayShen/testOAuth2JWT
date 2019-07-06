4115b8ad25d4dc97849895274e669cdcf90fbb1a
11591 <1159148321@qq.com>
2019-07-06 13:12:17 +0800

    draft self token login

--- src/main/java/com/test/testoauth2jwt/controller/UserController.java
+++ src/main/java/com/test/testoauth2jwt/controller/UserController.java
@@ -8,9 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private final SecretKey key;
+
+    public UserController() throws Exception {
+        key = getSecretEncryptionKey();
+    }
 
     @GetMapping("/login")
     public String login(UserLoginDto userLoginDto) throws Exception {
@@ -30,7 +41,41 @@ public class UserController {
         user.setRole("admin");
 
         String s = JSON.toJSONString(user);
-        return s;
+
+        byte[] bytes = encryptText(s, key);
+
+
+        String s1 = Base64.getEncoder().encodeToString(bytes);
+
+        return s1;
+    }
+
+    /**
+     * gets the AES encryption key. In your actual programs, this should be safely
+     * stored.
+     * @return
+     * @throws Exception
+     */
+    private SecretKey getSecretEncryptionKey() throws Exception{
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(128); // The AES key size in number of bits
+        SecretKey secKey = generator.generateKey();
+        return secKey;
+    }
+
+    /**
+     * Encrypts plainText in AES using the secret key
+     * @param plainText
+     * @param secKey
+     * @return
+     * @throws Exception
+     */
+    public byte[] encryptText(String plainText,SecretKey secKey) throws Exception{
+        // AES defaults to AES/ECB/PKCS5Padding in Java 7
+        Cipher aesCipher = Cipher.getInstance("AES");
+        aesCipher.init(Cipher.ENCRYPT_MODE, secKey);
+        byte[] byteCipherText = aesCipher.doFinal(plainText.getBytes());
+        return byteCipherText;
     }
 
 }
