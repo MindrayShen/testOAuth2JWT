6ca08d7d361983285cda2d2722391ce1572921d5
11591 <1159148321@qq.com>
2019-07-06 11:58:22 +0800

    first init

---
+++ src/main/java/com/test/testoauth2jwt/Testoauth2jwtApplication.java
@@ -0,0 +1,13 @@
+package com.test.testoauth2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testoauth2jwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testoauth2jwtApplication.class, args);
+    }
+
+}
